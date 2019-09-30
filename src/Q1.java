058e440ffef2cff49923a2abc00e79e0358b12d7
Khanh Do <khanh.x.do-1@ou.edu>
2019-09-30 14:02:59 -0500

    initial commit

---
+++ src/Q1.java
@@ -0,0 +1,15 @@
+
+public class Q1 {
+	// Q1: Is this a valid Java compiler?
+	// A: yes
+	// What is the difference between public and private fields?
+	// A: private fields: member method can access but class users cannot, public fields: class users can access
+	// 	  all operations to perform on the object.
+	
+	public int main() {
+		return 10;
+	}
+	public static void main (String[] args) {
+		
+	}
+}
